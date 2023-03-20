3cf2130785ef501eaaff82ecfd3dc423b50f435c
Mina Ji <106360156+MinaJi@users.noreply.github.com>
2023-03-20 23:47:14 +0900

    Update 점의 위치 구하기.js

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -9,3 +9,11 @@ function solution(dot) {
         return 4;
     }
 }
+
+// ^^d
+
+// function solution(dot) {
+//     const [num,num2] = dot;
+//     const check = num * num2 > 0;
+//     return num > 0 ? (check ? 1 : 4) : (check ? 3 : 2);
+// }
