205a2ffa290530c4e4c79115fd31e4d6e4ba4398
Mina Ji <106360156+MinaJi@users.noreply.github.com>
2023-03-20 22:18:26 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -0,0 +1,16 @@
+function solution(numer1, denom1, numer2, denom2) {
+    var answer = [];
+    let maxNum = 1;
+    let numer = numer1 * denom2 + numer2 * denom1;
+    let denom = denom1 * denom2
+    
+    for(let i=1; i<=numer; i++) {
+        if(numer%i === 0 && denom%i === 0) {
+            maxNum = i
+        }
+    }
+    
+    answer = [numer/maxNum, denom/maxNum]
+    
+    return answer;
+}
