31b6037f09b576296e12deaa0eefe5822a00e288
Mina Ji <106360156+MinaJi@users.noreply.github.com>
2023-03-28 23:25:56 +0900

    Update 구슬을 나누는 경우의 수.js

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -7,3 +7,17 @@ function factorial(n) {
 function solution(balls, share) {
     return Math.round(factorial(balls) / (factorial(balls - share) * factorial(share)));
 }
+
+// 반복문 말고 재귀 함수 사용
+
+// 팩토리얼을 재귀 함수로 구현
+// function factorial(n) {
+//   if (n === 0) {
+//     return 1;
+//   } else { 
+//     return n * factorial(n - 1);
+//   }
+// }
+
+// 더 간단하게 화살표 함수로 이렇게 함
+// const factorial = (n) => n === 0 ? 1 : n * factorial(num - 1)
