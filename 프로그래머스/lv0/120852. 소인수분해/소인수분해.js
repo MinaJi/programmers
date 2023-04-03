abd9e28d4d2c1554cb63ecc29d45db2227a98f9b
Mina Ji <106360156+MinaJi@users.noreply.github.com>
2023-04-03 21:59:54 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = [];
+    let i = 2;
+    
+    while (i <= n) {
+        if (n % i === 0) {
+            n = n / i;
+            answer.push(i);
+        } else {
+            i++;
+        }
+    }
+    
+    return [...new Set(answer)];
+}
