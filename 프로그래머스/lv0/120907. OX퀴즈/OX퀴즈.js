7ee937338fd96b1b85d1adb5e7a846b14af8216b
Mina Ji <106360156+MinaJi@users.noreply.github.com>
2023-04-12 23:07:32 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,10 @@
+function solution(quiz) {
+    var answer = [];
+    
+    for (let i = 0; i < quiz.length; i++) {
+        const equations = quiz[i];
+        const [equation, result] = equations.split(" = ");
+        answer.push(Function(`return (${equation}) === (${result}) ? "O": "X" `)());
+    }
+    return answer;
+}
