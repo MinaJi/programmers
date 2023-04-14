6e2d227e50c80fd90f959c3d22fbccb8dab623bc
Mina Ji <106360156+MinaJi@users.noreply.github.com>
2023-04-14 23:12:06 +0900

    Update 직사각형 넓이 구하기.js

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,8 +1,10 @@
 function solution(dots) {
-    const x = dots.map((dot) => dot[0]);
-    const y = dots.map((dot) => dot[1]);
-    const width = Math.max(...x) - Math.min(...x);
-    const height = Math.max(...y) - Math.min(...y);
+    const x = dots.map((dot) => dot[0]); // x좌표값들
+    const y = dots.map((dot) => dot[1]); // y좌표값들
+    const width = Math.max(...x) - Math.min(...x); // x의 길이
+    const height = Math.max(...y) - Math.min(...y); // y의 길이
     
     return width * height;
 }
+
+// x 가장 큰 값 - x 가장 작은 값 * y 가장 큰 값 - y 가장 작은 값
