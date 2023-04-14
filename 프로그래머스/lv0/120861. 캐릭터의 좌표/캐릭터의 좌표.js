84f8f1d7ec58c123038201958d9e6dd9dd4a7593
Mina Ji <106360156+MinaJi@users.noreply.github.com>
2023-04-15 00:00:10 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,23 @@
+function solution(keyinput, board) {
+    var answer = [0, 0];
+    const boardSize = [Math.floor(board[0] / 2), Math.floor(board[1] / 2)];
+    
+    keyinput.forEach((move) => {
+        switch (move) {
+            case "up":
+                if (!(answer[1] === boardSize[1])) answer[1]++;
+                break;
+            case "down":
+                if (!(answer[1] === -boardSize[1])) answer[1]--;
+                break;
+            case "left":
+                if (!(answer[0] === -boardSize[0])) answer[0]--;
+                break;
+            case "right":
+                if (!(answer[0] === boardSize[0])) answer[0]++;
+                break;
+        }
+    });
+    
+    return answer;
+}
